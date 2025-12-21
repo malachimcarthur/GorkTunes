52d92c418ef6bd9e68cdd1ecd36b9471f93e12fa
Malachi McArthur <malachi0mcarthur@gmail.com>
2025-12-20 22:13:05 -0600

    Initial Commit

---
+++ GorkTunes/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  GorkTunes
+//
+//  Created by Malachi McArthur on 12/20/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ GorkTunesTests/GorkTunesTests.swift
@@ -0,0 +1,17 @@
+//
+//  GorkTunesTests.swift
+//  GorkTunesTests
+//
+//  Created by Malachi McArthur on 12/20/25.
+//
+
+import Testing
+@testable import GorkTunes
+
+struct GorkTunesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
