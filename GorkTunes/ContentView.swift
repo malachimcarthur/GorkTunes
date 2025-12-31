86c5200311ff650d62e732f809b58668463be84e
Malachi McArthur <malachi0mcarthur@gmail.com>
2025-12-31 11:52:43 -0600

    create folder and select file no processing

--- GorkTunes/ContentView.swift
+++ GorkTunes/ContentView.swift
@@ -9,12 +9,15 @@ import SwiftUI
 import SwiftData
 import AVFAudio
 import Foundation
+import MediaPlayer
 
 struct ContentView: View {
     
     @State private var isPlaying = false
     @State private var musicTitle = "Monoco"
     @State private var player: AVAudioPlayer?
+    @State private var musicDirectoryURL: URL?
+    @State private var showFileImporter = false
     var body: some View {
         ZStack{
             Color.black.edgesIgnoringSafeArea(.all)
@@ -24,15 +27,30 @@ struct ContentView: View {
                     Image(systemName: (isPlaying ? "pause.circle.fill" : "play.circle.fill"))
                         .font(.largeTitle)
                         .onTapGesture {
-                            isPlaying = true
-                            playMusic()
+                            isPlaying ? pauseMusic() : playMusic()
+                        }
+                }
+                VStack(spacing:30){
+                    Image(systemName: "plus")
+                        .font(.largeTitle)
+                        .onTapGesture {
+                            showFileImporter = true
                         }
                 }
             }.onAppear(perform: prepareMusic)
-        }
+        }.onAppear(perform: getMusicDirectory)
+            .fileImporter(
+                isPresented: $showFileImporter,
+                allowedContentTypes: [.mp3],
+                allowsMultipleSelection: true,
+                onCompletion: {
+                    result in
+                }
+            )
     }
     
-
+    
+    
     private func prepareMusic(){
         guard let musicFile = Bundle.main.url(forResource: musicTitle, withExtension: "mp3")
         else{
@@ -56,15 +74,31 @@ struct ContentView: View {
     }
     private func playMusic(){
         player?.play()
-        print(player?.isPlaying ?? false)
+        isPlaying = true
+    }
+    private func pauseMusic(){
+        player?.stop()
+        isPlaying = false
+    }
+    private func getMusicDirectory(){
+        do{
+            let fileManager = FileManager.default
+            guard let musicDirectory = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first
+            else{
+                print("No directory found")
+                return
+            }
+            musicDirectoryURL = musicDirectory.appendingPathComponent("GorkTunesMusic", isDirectory: true)
+            if !fileManager.fileExists(atPath: (musicDirectoryURL?.path())!){
+                try fileManager.createDirectory(at: musicDirectoryURL ?? URL.documentsDirectory, withIntermediateDirectories: true, attributes: nil)
+                print("file created at path \(musicDirectoryURL?.path() ?? "No Path")")
+            }else{
+                print("file exists at path \(musicDirectoryURL?.path() ?? "No Path")")
+            }
+        }
+        catch{
+            print("error in getMusic: \(error.localizedDescription)")
+        }
     }
 }
-//
-//  Player.swift
-//  GorkTunes
-//
-//  Created by Malachi McArthur on 12/29/25.
-//
-
-
 
